ff428c4edcaa965ccf7b4f8a472e32ae3ed03dc7
Daria Brinza <dahis.br@gmail.com>
2025-08-28 18:24:29 +0300

    refactor hook for Pagination to be able to split a large section into smaller and devide it in two pages

--- src/app/(main)/resume-editor/components/ResumePreview/hooks/usePaginateDom.ts
+++ src/app/(main)/resume-editor/components/ResumePreview/hooks/usePaginateDom.ts
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useLayoutEffect } from "react";
 
 type Page = HTMLElement[];
 
@@ -9,34 +9,65 @@ type PaginatedDomProps = {
 const usePaginateDom = ({ ref }: PaginatedDomProps) => {
   const [pages, setPages] = React.useState<Page[]>([]);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     if (!ref.current) return;
 
-    const children = Array.from(ref.current.children) as HTMLElement[]; // Same as [...ref.current.children]
+    requestAnimationFrame(() => {
+      const children = Array.from(ref.current!.children) as HTMLElement[]; // Same as [...ref.current.children]
 
-    const newPages: Page[] = [];
-    let currentPage: Page = [];
-    let currentHeight = 0;
+      const newPages: Page[] = [];
+      let currentPage: Page = [];
+      let currentHeight = 0;
 
-    for (const child of children) {
-      const childHeight = child.offsetHeight;
+      for (const child of children) {
+        const childHeight = child.offsetHeight;
 
-      if (childHeight + currentHeight > 1123) {
-        newPages.push(currentPage);
-        currentPage = [child];
-        currentHeight = childHeight;
-      } else {
-        currentHeight += childHeight;
-        currentPage.push(child);
+        if (childHeight + currentHeight < 1123) {
+          currentHeight += childHeight;
+          currentPage.push(child);
+        } else {
+          // TODO: check if the elemnt has only one child inside, then try to split the text inside
+
+          // Высота верхних блоков + то что мы постепенно добавляем в цикле ниже
+          const innerChildren = Array.from(child.children) as HTMLElement[];
+
+          let wrapper = child.cloneNode(false) as HTMLElement;
+          let tempHeight = currentHeight;
+
+          const tempWrappernodes = [];
+
+          for (const [idx, innerChild] of innerChildren.entries()) {
+            const innerChildHeight = innerChild.offsetHeight;
+
+            if (tempHeight + innerChildHeight > 1123) {
+              if (wrapper.children.length > 0) {
+                currentPage.push(wrapper);
+              }
+
+              newPages.push(currentPage);
+              currentPage = [];
+              currentHeight = 0;
+              tempHeight = 0;
+              wrapper = child.cloneNode(false) as HTMLElement;
+            }
+
+            tempWrappernodes.push(innerChild.cloneNode(true));
+            wrapper.appendChild(innerChild.cloneNode(true));
+            tempHeight += innerChildHeight;
+            // currentHeight += innerChildHeight;
+
+            if (idx === innerChildren.length - 1) {
+              currentPage.push(wrapper);
+            }
+          }
+        }
       }
 
-      // console.log({ childHeight, currentHeight, currentPage });
-    }
-
-    if (currentPage.length > 0) {
-      newPages.push(currentPage);
-    }
-    setPages(newPages);
+      if (currentPage.length > 0) {
+        newPages.push(currentPage);
+      }
+      setPages(newPages);
+    });
   }, [ref]);
 
   console.log({ pages });
